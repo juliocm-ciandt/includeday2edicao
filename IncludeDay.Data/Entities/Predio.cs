0fcd7011066939d456be7da4682020b1e1d17424
Rodrigo Sclosa <skywan@globo.com>
2015-08-11 20:39:14 -0300

    Adição da classe Predio.

---
+++ IncludeDay.Data/Entities/Predio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace IncludeDay.Data.Entities
+{
+    /// <summary>
+    /// Prédios dos locais
+    /// </summary>
+    [Serializable]
+    public class Predio
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Descricao { get; set; }
+    }
+}
+
