5a05c7d831d2dda5607be9b60583b86b20577e6c
Rodrigo Sclosa <skywan@globo.com>
2015-08-11 21:24:36 -0300

    Exemplo de popular o select por JSON.

---
+++ IncludeDay.Sample/js/exemplo_select.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+
+    var URL_PREDIO = "http://localhost/IncludeDay.Services/api/Predio";
+
+    $select = $('#predios');
+
+    $.ajax({
+        url: URL_PREDIO,
+        dataType:'JSON',
+        success:function(data){
+            $select.html('');
+            $.each(data, function(key, val) {
+                $select.append('<option id="' + val.Id + '">' + val.Nome + '</option>');
+            })
+        },
+        error:function(){
+            $select.html('<option id="-1">Nenhum disponível</option>');
+        }
+    });
+});
